8e78e281306635009583d5ed908ace506262c203
ramkrishna.jana <ramkrishna.jana@42hertz.com>
2017-06-19 11:46:49 +0530

    https://github.com/ram42H/FdxOpportunityClosed.git

---
+++ FdxOpportunityClosed/CRMQueryExpression.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FdxOpportunityClosed
+{
+    class CRMQueryExpression
+    {
+        string filterAttribute;
+        ConditionOperator filterOperator;
+        object filterValue;
+
+        public CRMQueryExpression(string _attr, ConditionOperator _opr, object _val)
+        {
+            this.filterAttribute = _attr;
+            this.filterOperator = _opr;
+            this.filterValue = _val;
+        }
+        public static QueryExpression getQueryExpression(string _entityName, ColumnSet _columnSet, CRMQueryExpression[] _exp)
+        {
+            QueryExpression query = new QueryExpression();
+            query.EntityName = _entityName;
+            query.ColumnSet = _columnSet;
+            query.Distinct = false;
+            query.Criteria = new FilterExpression();
+            query.Criteria.FilterOperator = LogicalOperator.And;
+            for (int i = 0; i < _exp.Length; i++)
+            {
+                query.Criteria.AddCondition(_exp[i].filterAttribute, _exp[i].filterOperator, _exp[i].filterValue);
+            }
+
+            return query;
+        }
+
+        public static int GetOptionsetValue(IOrganizationService _service, string _optionsetName, string _optionsetSelectedText)
+        {
+            int optionsetValue = 0;
+            try
+            {
+                RetrieveOptionSetRequest retrieveOptionSetRequest =
+                    new RetrieveOptionSetRequest
+                    {
+                        Name = _optionsetName
+                    };
+
+                // Execute the request.
+                RetrieveOptionSetResponse retrieveOptionSetResponse =
+                    (RetrieveOptionSetResponse)_service.Execute(retrieveOptionSetRequest);
+
+                // Access the retrieved OptionSetMetadata.
+                OptionSetMetadata retrievedOptionSetMetadata = (OptionSetMetadata)retrieveOptionSetResponse.OptionSetMetadata;
+
+                // Get the current options list for the retrieved attribute.
+                OptionMetadata[] optionList = retrievedOptionSetMetadata.Options.ToArray();
+                foreach (OptionMetadata optionMetadata in optionList)
+                {
+                    //If the value matches/....
+                    if (optionMetadata.Label.UserLocalizedLabel.Label.ToString() == _optionsetSelectedText)
+                    {
+                        optionsetValue = (int)optionMetadata.Value;
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return optionsetValue;
+        }
+        public static string GetOptionsSetTextForValue(IOrganizationService service, string entityName, string attributeName, int selectedValue)
+        {
+
+            RetrieveAttributeRequest retrieveAttributeRequest = new
+            RetrieveAttributeRequest
+            {
+                EntityLogicalName = entityName,
+                LogicalName = attributeName,
+                RetrieveAsIfPublished = true
+            };
+            // Execute the request.
+            RetrieveAttributeResponse retrieveAttributeResponse = (RetrieveAttributeResponse)service.Execute(retrieveAttributeRequest);
+            // Access the retrieved attribute.
+            Microsoft.Xrm.Sdk.Metadata.PicklistAttributeMetadata retrievedPicklistAttributeMetadata = (Microsoft.Xrm.Sdk.Metadata.PicklistAttributeMetadata)
+            retrieveAttributeResponse.AttributeMetadata;// Get the current options list for the retrieved attribute.
+            OptionMetadata[] optionList = retrievedPicklistAttributeMetadata.OptionSet.Options.ToArray();
+            string selectedOptionLabel = null;
+            foreach (OptionMetadata oMD in optionList)
+            {
+                if (oMD.Value == selectedValue)
+                {
+                    selectedOptionLabel = oMD.Label.LocalizedLabels[0].Label.ToString();
+                    break;
+                }
+            }
+            return selectedOptionLabel;
+        }
+    }
+}
---
+++ FdxOpportunityClosed/ValidateOpportunityClosedAsWon.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FdxOpportunityClosed
+{
+    public class ValidateOpportunityClosedAsWon : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //Extract the tracing service for use in debugging sandboxed plug-ins....
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            //Obtain execution contest from the service provider....
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            int step = 0;
+            Guid opportunityId = Guid.Empty;
+
+            if (context.InputParameters.Contains("OpportunityClose") && context.InputParameters["OpportunityClose"] is Entity)
+            {
+                step = 1;
+                Entity opportunityEntity = (Entity)context.InputParameters["OpportunityClose"];
+
+                //if (opportunityEntity.LogicalName != "OpportunityClose")
+                //    return;
+
+                try
+                {
+                    step = 2;
+                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                    //Get current user information....
+                    step = 3;
+                    WhoAmIResponse response = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
+
+                    step = 4;
+                    if (opportunityEntity.Attributes.Contains("opportunityid") && opportunityEntity.Attributes["opportunityid"] != null)
+                    {
+                        EntityReference entityRef = (EntityReference)opportunityEntity.Attributes["opportunityid"];
+ 
+                        if (entityRef.LogicalName == "opportunity")
+                        {
+                            opportunityId = entityRef.Id;
+                        }
+                    } 
+
+                    step = 4;
+                    QueryExpression oppProductQuery = CRMQueryExpression.getQueryExpression("opportunityproduct", new ColumnSet("opportunityid", "opportunityproductid"), new CRMQueryExpression[] { new CRMQueryExpression("opportunityid", ConditionOperator.Equal, opportunityId) });
+                    step = 5;
+                    EntityCollection opportunityProduct = service.RetrieveMultiple(oppProductQuery);
+                    step = 6;
+                    if (!(opportunityProduct.Entities.Count > 0))
+                        throw new InvalidPluginExecutionException(OperationStatus.Failed, "Please add products in the opportunity before closing it as won. \n\n\n");
+
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException(string.Format("An error occurred in the ValidateOpportunityClosedAsWon plug-in at Step {0}.", step), ex);
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("ValidateOpportunityClosedAsWon: step {0}, {1}", step, ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
